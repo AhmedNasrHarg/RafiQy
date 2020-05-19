fb1bdd5a9e954e4413460526c6cac3e12aae53a5
Nadeen Amasha <42452448+nadeen97@users.noreply.github.com>
2020-05-19 23:49:04 +0200

    changes ios

--- fluttershare/fluttershare-master/ios/Flutter/flutter_export_environment.sh
+++ fluttershare/fluttershare-master/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\cbtproject\fluttershare\fluttershare-master"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Volumes/W/NadeenITI/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/W/NadeenITI/Graduation_project/cbt_app/fluttershare/fluttershare-master"
+export "FLUTTER_TARGET=/Volumes/W/NadeenITI/Graduation_project/cbt_app/fluttershare/fluttershare-master/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=/Volumes/W/NadeenITI/Flutter/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
