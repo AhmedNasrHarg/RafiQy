e769eda0be850e2eefbc9c88893148e1ffb2c012
Ahmed Nasr <ahmed.harg@yahoo.com>
2020-05-23 02:24:46 +0200

    sheet-up

--- fluttershare/fluttershare-master/lib/main.dart
+++ fluttershare/fluttershare-master/lib/main.dart
@@ -11,41 +11,38 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fluttershare/routes/custom_router.dart';
 
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  static void setLocale (BuildContext context,Locale locale)
-  {
-    _MyAppState state =context.findAncestorStateOfType<_MyAppState>();
+  static void setLocale(BuildContext context, Locale locale) {
+    _MyAppState state = context.findAncestorStateOfType<_MyAppState>();
     state.setLocal(locale);
   }
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-   Locale _locale;
+  Locale _locale;
 
-  void setLocal(Locale locale)
-  {
+  void setLocal(Locale locale) {
     setState(() {
-      _locale=locale;
+      _locale = locale;
     });
   }
 
   @override
   void didChangeDependencies() {
-getLocale().then((locale){
-  setState(() {
-    this._locale=locale;
-  });
-});
-super.didChangeDependencies();
-
-}
+    getLocale().then((locale) {
+      setState(() {
+        this._locale = locale;
+      });
+    });
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,23 +52,18 @@ super.didChangeDependencies();
           child: CircularProgressIndicator(),
         ),
       );
-    }
-    else{
-    return MaterialApp(
-      title: 'FlutterShare',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          accentColor: Colors.teal // secondryColor
-          ),
-      home: Sheets(),
-      ),
-      home: Home(),
-       locale: _locale,
-        supportedLocales: [
-          Locale('en', 'US'),
-          Locale('ar', 'SA')
-        ],
+    } else {
+      return MaterialApp(
+        title: 'FlutterShare',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primarySwatch: Colors.deepPurple,
+            accentColor: Colors.teal // secondryColor
+            ),
+
+        home: Sheets(),
+        locale: _locale,
+        supportedLocales: [Locale('en', 'US'), Locale('ar', 'SA')],
         localizationsDelegates: [
           DemoLocalization.delegate,
           GlobalMaterialLocalizations.delegate,
@@ -81,8 +73,7 @@ super.didChangeDependencies();
         localeResolutionCallback: (deviceLocale, supportLocales) {
           for (var locale in supportLocales) {
             if (locale.languageCode == deviceLocale.languageCode &&
-                locale.countryCode == deviceLocale.countryCode
-            ) {
+                locale.countryCode == deviceLocale.countryCode) {
               return deviceLocale;
             }
           }
@@ -90,7 +81,7 @@ super.didChangeDependencies();
         },
         onGenerateRoute: CustomRouter.allRoutes,
         // initialRoute: homeRoute,
-    );
-  }
+      );
+    }
   }
 }
